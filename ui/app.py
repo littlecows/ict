6ae60c9a901f52531dec7dd0eeb27f6125b486a5
littlecows <bfbz800@gmail.com>
2024-07-04 13:03:17 +0700

    moking up done

--- ui/app.py
+++ ui/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/personal')
+def personal():
+    return render_template("personal.html")
+
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
